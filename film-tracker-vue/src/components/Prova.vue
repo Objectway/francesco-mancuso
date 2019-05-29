766a552c418f9134fd4e94eceb2bb1a506993984
framancuso <f.mancuso97@hotmail.it>
2019-05-29 23:25:28 +0200

    1

---
+++ film-tracker-vue/src/components/Prova.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      CIAO
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import FilmDetails from './FilmDetails.vue'
+@Component({
+  components: {
+  },
+})
+export default class Prova extends Vue {
+  created(){
+      this.$router.push('/')
+  }}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
